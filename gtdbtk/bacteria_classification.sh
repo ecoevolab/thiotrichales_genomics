4e248456ef5f88da4810ed55adfc2e5887064159
Mónica Reyes Ramírez <mreyesramirez2005@gmail.com>
2026-07-08 13:34:30 -0600

    Reordenamiento de los archivos

--- gtdbtk/bacteria_classification.sh
+++
@@ -1,8 +0,0 @@
-echo -e "genome_id\tclassification" > gtdbtk_classification.csv
-
-for d in */; do
-  file=$(ls "$d"/*.bac120.summary.tsv 2>/dev/null)
-  if [ -f "$file" ]; then
-    awk -F'\t' 'NR==2 {print $1 "\t" $2}' "$file"
-  fi
-done >> gtdbtk_classification.csv
